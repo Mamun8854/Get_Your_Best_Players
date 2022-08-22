0d9875ce69e388bfe700f512711db02811b87d59
Mamun8854 <mamunabdullah8854@gmail.com>
2022-08-22 13:53:07 +0600

    player select section complete

--- js/index.js
+++ js/index.js
@@ -30,16 +30,20 @@ function selectButton(element) {
   playerArray.push(playerObject);
   const playerLegnth = playerArray.length;
 
-  if (playerArray.length < 6) {
+  if (playerArray.length <= 5) {
     document.getElementById("selected-player-list").innerText = playerLegnth;
     selectedPlayerBody(playerArray);
     element.setAttribute("disabled", "");
     element.style.backgroundColor = "red";
   } else {
     alert("maximum 5 player you can add in your list");
-    // let allCard = document.querySelector(".card-button");
-    // allCard.setAttribute("disabled", "");
-    // allCard.style.backgroundColor = "red";
+    let allCard = document.querySelectorAll(".card-button");
+    for (let i = 0; i < allCard.length; i++) {
+      allCard[i].setAttribute("disabled", "");
+      allCard[i].style.backgroundColor = "red";
+    }
   }
 }
 // Select Player Code End Here
+
+// Calculation Code Satart Herer
